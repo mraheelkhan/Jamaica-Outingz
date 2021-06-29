da4497b173bb18f58c1a104fdbaca84d25458959
Ihtisham Ahmad <ihtisham467@gmail.com>
2021-06-30 01:00:48 +0500

    tours api

--- app/Http/Resources/TourResource.php
+++ app/Http/Resources/TourResource.php
@@ -17,18 +17,13 @@ class TourResource extends JsonResource
         // return parent::toArray($request);
         return [
             'id' => $this->id,
-            'tour_image' => 'https://www.funtoursjamaica.com/images/custom_img/slider/2.jpg',
             'tour_name' => $this->tour_name,
             'tour_location' => $this->location,
             'cost' => $this->cost,
             'duration' => $this->duration,
             'guide_info' => $this->guide_info,
-            'tour_background_image' => 'https://www.funtoursjamaica.com/images/custom_img/slider/2.jpg',
-            'tour_images_list' => [
-                'image_1' => 'https://www.funtoursjamaica.com/images/custom_img/slider/3.jpg',
-                'image_2' => 'https://www.funtoursjamaica.com/images/custom_img/slider/4.jpg',
-                'image_3' => 'https://www.funtoursjamaica.com/images/custom_img/slider/2.jpg',
-            ],
+            'tour_background_image' => $this->img,
+            'tour_images_list' => $this->images,
         ];
     }
 }

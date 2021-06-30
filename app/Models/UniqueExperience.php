7510fc6f7994d75f7c8589caf32003843a033f36
Ihtisham Ahmad <ihtisham467@gmail.com>
2021-07-01 00:54:57 +0500

    change name to link

--- app/Models/UniqueExperience.php
+++ app/Models/UniqueExperience.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,6 +11,11 @@ class UniqueExperience extends Model
     use HasFactory;
     protected $guarded = ['id'];
     protected $hidden = ['created_at', 'updated_at'];
+
+    protected $appends = ['background_image'];
+    public function getBackgroundImageAttribute() {
+        return asset('images/unique-experiences/'.$this->cover_image);
+    }
 
     public function images() {
         return $this->hasMany('App\Models\UniqueExperienceImage', 'unique_experience_id');

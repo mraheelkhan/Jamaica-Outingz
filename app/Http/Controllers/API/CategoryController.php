6f7940c4d26088e433fa42cb496f968577a84a0e
Ihtisham Ahmad <ihtisham467@gmail.com>
2021-07-09 01:30:46 +0500

    category items image link issue

--- app/Http/Controllers/API/CategoryController.php
+++ app/Http/Controllers/API/CategoryController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\Category;
 use App\Models\Item;
 use App\Http\Resources\CategoryResourceCollection;
+use App\Http\Resources\ItemResourceCollection;
 
 class CategoryController extends Controller
 {
